2b1803ba36d0188caece89c9d7a1228f3c46a3d9
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-10-30 14:40:33 -0600

    added to add a new user to the blockchain with web3.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,178 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super (props);
+
+    const CryptoPongHero = window.web3.eth.contract ([
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_walletAddress",
+            "type": "address"
+          },
+          {
+            "name": "_userName",
+            "type": "string"
+          }
+        ],
+        "name": "_createUser",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "previousOwner",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "name": "newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "userIdToAddress",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "users",
+        "outputs": [
+          {
+            "name": "id",
+            "type": "address"
+          },
+          {
+            "name": "userName",
+            "type": "string"
+          },
+          {
+            "name": "wins",
+            "type": "uint32"
+          },
+          {
+            "name": "losses",
+            "type": "uint32"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ]);
+
+    this.state = {
+      ContractInstance: CryptoPongHero.at ('0x1375130b4e05af77cf6e3c76f379bd3ed2204803'),
+      userName: ""
+    }
+
+    this.createUser = this.createUser.bind (this);
+  }
+
+  createUser (event)  {
+    event.preventDefault();
+
+    const { _createUser } = this.state.ContractInstance;
+    const {userName: newState} = this.state;
+
+
+    _createUser(
+      window.web3.eth.accounts[0],
+      newState,
+      (err, result) =>  {
+        console.log("user is being added to blockchain");
+      } 
+    )
+  }
+  
+
+
+  handleChange = (e) => {
+    e.preventDefault()
+    const target = e.target
+    const value = target.value
+    const name = target.name
+
+    this.setState({
+        [name]: value
+    })
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title"> Crypto Pong Hero Create User</h1>
         </header>
+          <br />
+          <br />
+          <form className="job-form" onSubmit={this.createUser}>
+            <label htmlFor="userName">User Name</label>
+            <input type="text" name="userName" value={this.state.userName} onChange={event => this.setState({userName: event.target.value})}/>
+            <input type="submit" name="submit" value="Submit" />
+            </form>
+          <br />
+          <br />
       </div>
     );
   }
